7f2a2eaaec4261f1adc30969c3dcd7feb6a06e5a
justinobney <justin@envoc.com>
2013-12-11 11:16:31 -0600

    Entity framework is already caching this

--- Jobney.EF.Learning/Data/DataContext.cs
+++ Jobney.EF.Learning/Data/DataContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Jobney.Core.Domain;
@@ -11,12 +9,8 @@ namespace Jobney.EF.Learning.Data
 {
     public class DataContext : DbContext, IDbContext
     {
-        private readonly Dictionary<Type, object> _dbSets;
-
-        public DataContext()
-            : base("DefaultConnection")
+        public DataContext() : base("DefaultConnection")
         {
-            _dbSets = new Dictionary<Type, object>();
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder) {
@@ -25,13 +19,7 @@ namespace Jobney.EF.Learning.Data
         }
 
         public new IDbSet<TEntity> Set<TEntity>() where TEntity : Entity {
-            if (_dbSets.ContainsKey(typeof (TEntity)))
-                return _dbSets[typeof (TEntity)] as IDbSet<TEntity>;
-
-            var dbSet = base.Set<TEntity>();
-            _dbSets.Add(typeof (TEntity), dbSet);
-
-            return dbSet;
+            return base.Set<TEntity>(); ;
         }
 
         public new DbEntityEntry Entry<TEntity>(TEntity entity) where TEntity : Entity {
