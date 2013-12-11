a761e64590f2f9d70ab3e01e4e5e012a31c3c849
Justin Obney <justinobney@gmail.com>
2013-12-10 21:14:06 -0600

    Initial commit - Good starting point

---
+++ Jobney.EF.Learning/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Jobney.EF.Learning
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Jobney.EF.Learning/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jobney.EF.Learning.MvcApplication" Language="C#" %>
