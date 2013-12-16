5bb40d887e0e80baad6df64c5071f4d97409705b
justinobney <justin@envoc.com>
2013-12-16 08:47:39 -0600

    Playing with extension methods and testing

---
+++ Jobney.EF.Learning/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobney.EF.Learning.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string slugify(this string str)
+        {
+            return string.Join("-", str.ToLower().Split(' '));
+        }
+    }
+}
---
+++ Jobney.Tests/StringExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Envoc.Core.UnitTests.Extensions;
+using Jobney.EF.Learning.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobney.EF.UnitTests.StringExtensionsContainer
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        private string target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+
+        [TestMethod]
+        public void StringSlugify_SlugifiesAString()
+        {
+            //arrange
+            target = "This is not a test";
+
+            //act
+            var result = target.slugify();
+
+            //assert
+            const string expected = "this-is-not-a-test";
+
+            result.ShouldBe(expected);
+        }
+    }
+}
