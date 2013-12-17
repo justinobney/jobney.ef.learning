eb89e0c3b8fc8f90c1f4b3e1bf837b662174d651
Justin Obney <justinobney@gmail.com>
2013-12-16 23:06:40 -0600

    seperating viewmodel to file

---
+++ Jobney.EF.Learning/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Jobney.EF.Learning.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
