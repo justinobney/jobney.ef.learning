fc06159d7649bf79fc776aad5e0391a60d4fb927
Justin Obney <justinobney@gmail.com>
2013-12-17 23:15:15 -0600

    adding interceptors to set token and redirect to login on 401

--- Jobney.EF.Learning/Scripts/App/App1/_module.js
+++ Jobney.EF.Learning/Scripts/App/App1/_module.js
@@ -1,4 +1,4 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
     var app = angular.module('Learning.App1', [
@@ -8,11 +8,31 @@
         'ngStorage'
     ]);
 
-    app.config([function() {
-        
+    app.factory('ApiResponseInterceptor', ['$q', '$location', 'AppState',
+        function ($q, $location, AppState) {
+            return {
+                request: function (config) {
+                    if (AppState.token && AppState.token.Key)
+                        config.headers["X-Api-Token"] = AppState.token.Key;
+                    
+                    return config;
+                },
+                responseError: function (rejection) {
+                    if (rejection.status === 401) {
+                        $location.path('#/login');
+                    }
+                    return $q.reject(rejection);
+                }
+            };
+        }]);
+
+    //Http Intercpetor to check auth failures for xhr requests
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
+        $httpProvider.interceptors.push('ApiResponseInterceptor');
     }]);
 
-    app.run([function() {
+    app.run([function () {
 
     }]);
 })();
